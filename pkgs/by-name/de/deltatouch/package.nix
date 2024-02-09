7b798fb7b06450e7f31693e9b4d34a38f4148088
link2xt <link2xt@testrun.org>
2024-02-09 20:08:30 +0000

    Update deltatouch source to include <array>

--- pkgs/by-name/de/deltatouch/package.nix
+++ pkgs/by-name/de/deltatouch/package.nix
@@ -18,8 +18,8 @@ stdenv.mkDerivation rec {
     domain = "codeberg.org";
     owner = "lk108";
     repo = "deltatouch";
-    rev = "6e2805515b9e3a3bbdf4871eda2a0fe65500aa1d";
-    hash = "sha256-jCoT4AdI8XOBhvtNsNdweLQb8XRD3YnDpnx4VRFHfN0=";
+    rev = "7b5e27044f64c5a92ca0493251d78d6663eba3f9";
+    hash = "sha256-Jrr/TkfBp/vvGhrbVP+ZEUyNg3hiWRAQHD79HddpY0U=";
     fetchSubmodules = true;
   };
 
@@ -37,9 +37,6 @@ stdenv.mkDerivation rec {
     substituteInPlace CMakeLists.txt --replace "set(DATA_DIR /)" "set(DATA_DIR $out/share)"
     substituteInPlace plugins/DeltaHandler/CMakeLists.txt --replace '/lib/' '${placeholder "out"}/lib/'
     substituteInPlace plugins/DTWebEngineProfile/CMakeLists.txt --replace '/lib/' '${placeholder "out"}/lib/'
-    echo "#include <array>" > tmp
-    cat tmp plugins/DeltaHandler/deltahandler.cpp > tmp2
-    mv tmp2 plugins/DeltaHandler/deltahandler.cpp
     mkdir -p build/plugins/DeltaHandler
     cp --no-preserve=mode ${libdeltachat}/lib/libdeltachat.so build/plugins/DeltaHandler/libdeltachat.so
     cp --no-preserve=mode ${quirc}/lib/libquirc.so.1.0 build/plugins/DeltaHandler/libquirc.so.1.2
